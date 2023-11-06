8530b2fa080c9c5c7d41e9a0ac2f68b7acca6a04
Edward Carter <146713869+edward-fc@users.noreply.github.com>
2023-11-06 15:21:25 +0000

    finished task 3 of Week6

--- Week6/grades.py
+++ Week6/grades.py
@@ -1,10 +1,35 @@
 list = []
-with open("my_newfile", "r") as fileObject:
-    # Read a line form the file
+# Open a file using the with feature
+with open("Week6/grades.txt", "r") as fileObject:
+    while True:
+        # Read a line form the file
         line = fileObject.readline()
         # If the line is empty break the while loop
         if line == "":
             break
         else:
-            list.append(line)
+            if line[:-2] == "\n":
+                line = line[:-2]
+            list.append(int(line))
 print(list)
+print(len(list))
+#max grade
+max=0
+mini = 100
+moyen = 0
+for element in list:
+    moyen+=element
+    if element > max:
+        max = element
+    if element < mini:
+        mini = element
+print(max)
+print(mini)
+print(moyen/len(list))
+list.sort()
+median=0
+if len(list)%2:
+    median = (list[len(list)/2]+list[len(list)/2+1])/2
+else:
+    median = list[round(len(list)/2)]/2
+print(median)
